e6bb9a2ad67f318f35bdb45c5c5a598aee223741
Étudiant <oswaldessongue@gmail.com>
2024-01-10 09:19:42 -0500

    ajout bibliotheque.h et main.cpp

---
+++ bibliotheque.h
@@ -0,0 +1,26 @@
+//
+// Created by Étudiant on 2024-01-10.
+//
+
+#ifndef BIBLIOTHEQUE_MAIN_H
+#define BIBLIOTHEQUE_MAIN_H
+
+#include <iostream>
+using namespace std;
+
+//une bibliotheque contient une liste de livres
+class Bibliotheque{
+
+    private:
+        //une liste de livres
+
+
+
+    public:
+        afficherLaBiblio();
+
+
+    };
+
+
+#endif //BIBLIOTHEQUE_MAIN_H
---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Étudiant on 2024-01-10.
+//
+
+#include "bibliotheque.h"
+
+int main(){
+
+    cout << "Hello Hasna et Oswald";
+
+    return 0;
+}
