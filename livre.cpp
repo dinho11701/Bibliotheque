58634d5c08b87424ba3459d199e6439e60adff19
Étudiant <oswaldessongue@gmail.com>
2024-01-10 10:02:28 -0500

    ajout bonFormatAuteur() ds livre.cpp

---
+++ livre.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Étudiant on 2024-01-10.
+//
+
+#include "livre.h"
+
+bool Livre::bonFormatAuteur() {
+
+    //exemple: Harry Potter
+    //exemle: Oswald
+    //1ere lettre de chaque mot en maj
+
+    istringstream iss(auteur);
+    string word;
+    bool valide = true;
+
+    while(iss >> word && valide){
+
+        if( !(isupper(word[0])) ){
+            valide = false;
+        }
+
+    }
+
+}
+
+bool Livre::bonFormatTitre() {
+
+
+    //exemple: Harry et la coupe des sorciers
+
+
+    return true;
+}
